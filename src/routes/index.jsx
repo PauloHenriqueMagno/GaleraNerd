4d851498e97df8c0fb3fa8ba6ac2f2de2f455fb7
PauloHenriqueMagno <paulohm2309@gmail.com>
2021-11-09 18:34:12 -0300

    feat: basic files

---
+++ src/routes/index.jsx
@@ -0,0 +1,12 @@
+import { Switch, Route } from "react-router";
+
+const Routes = () => {
+    return (
+        <Switch>
+            <Route path="/">
+            </Route>
+        </Switch>
+    );
+};
+
+export default Routes;
