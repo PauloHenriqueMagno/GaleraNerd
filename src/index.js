b231d6d67fb0c16a814914f53f3b3609577c2260
pedromenimen <pedromenimen@gmail.com>
2021-11-09 20:47:15 -0300

    feat: input and some theme configuration

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 import { BrowserRouter } from "react-router-dom";
-import Provider from './providers';
+import Provider from "./providers";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./Styles";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider>
       <BrowserRouter>
-        <App />
+        <ChakraProvider theme={theme}>
+          <App />
+        </ChakraProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
