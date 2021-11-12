b2207dfa06accbceb3c54311c8433cfb419a6a8f
Matheus-Henrique-Buey <matheushbuei.99@gmail.com>
2021-11-12 14:13:07 -0300

    page Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,35 @@
+import { Box, Heading, Text, Button } from "@chakra-ui/react";
+import backgrond from "../../assets/wallpaper.png";
+
 const Home = () => {
   return (
-    <>
-    Home
-    </>
+    <Box>
+      <Box
+        backgroundImage={backgrond}
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
+        backgroundSize="cover"
+        color="white"
+        p="50px 20px"
+      >
+        <Heading mb="4">
+          GALERA NERD
+        </Heading>
+        <Text fontSize="16px" >
+          rem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500since
+        </Text>
+        <Button 
+          mt="4" 
+          p="6"
+          w="150px"
+          bg="purple.2" color="white" 
+          _hover={{backgroundColor: "purple.4"}}
+        >
+          Saiba Mais
+        </Button>
+      </Box>
+    </Box>
   )
 }
 
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,7 @@
 import { Switch, Route } from "react-router";
 import Register from "../pages/Register";
 import Login from "../pages/login";
+import Home from "../pages/Home";
 
 const Routes = () => {
     return (
@@ -12,6 +13,7 @@ const Routes = () => {
                 <Login />
             </Route>
             <Route path="/">
+                <Home />
             </Route>
         </Switch>
     );
