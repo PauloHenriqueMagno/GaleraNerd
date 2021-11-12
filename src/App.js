2e36e2cf621c5b4f39c37fe6020bdf1660824b64
Paulo Henrique Magno <79842305+PauloHenriqueMagno@users.noreply.github.com>
2021-11-12 09:23:26 -0300

    feat: add routes to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import "./App.css";
-import Login from "./pages/login";
 import Routes from "./routes";
 
 function App() {
-  return <Login />;
-
-  /* <Routes /> */
-}
+  return <Routes />;
+};
 
 export default App;
