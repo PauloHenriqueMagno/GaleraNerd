50b840b8dec25babc84466ebcd342ddb1c1c689d
Sidney Patez <instalers.sa@gmail.com>
2021-11-13 14:57:22 -0300

    feat: text area input

---
+++ src/components/TextAreaInput/index.jsx
@@ -0,0 +1,29 @@
+import { Textarea, Text } from "@chakra-ui/react";
+
+const TextAreaInput = ({
+  register,
+  registerName,
+  errorMessage,
+  placeholder,
+  ...rest
+}) => {
+  return (
+    <>
+      <Textarea
+        placeholder={placeholder}
+        size="lg"
+        isInvalid={!!errorMessage}
+        {...register(registerName)}
+        {...rest}
+        errorBorderColor="error"
+        focusBorderColor={!!errorMessage ? "error" : "grey.2"}
+        bg="grey.4"
+        _placeholder={{ color: "grey.2" }}
+        rows="7"
+      />
+      {!!errorMessage && <Text>{errorMessage}</Text>}
+    </>
+  );
+};
+
+export default TextAreaInput;
