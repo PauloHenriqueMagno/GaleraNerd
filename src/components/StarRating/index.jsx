a2c5bce0b59039d9e8f4f4ee5ad4fd84d6c4d62e
Sidney Patez <instalers.sa@gmail.com>
2021-11-15 11:33:41 -0300

    feat: star rating feature

---
+++ src/components/StarRating/index.jsx
@@ -0,0 +1,51 @@
+import { FaStar } from "react-icons/fa";
+import { Flex, Icon } from "@chakra-ui/react";
+import { useState } from "react";
+
+const StarRating = ({
+  register = () => {},
+  name,
+  disabled = false,
+  defaultValue = 0,
+  ...rest
+}) => {
+  const [rating, setRating] = useState(defaultValue);
+  const [hover, setHover] = useState(null);
+
+  const handleRating = (value) => {
+    if (!disabled) {
+      setRating(value);
+    }
+  };
+
+  return (
+    <Flex w="100%" justifyContent="space-between">
+      {[...Array(5)].map((star, index) => {
+        const ratingValue = index + 1;
+
+        return (
+          <label key={ratingValue}>
+            <input
+              type="radio"
+              value={ratingValue}
+              name={name}
+              {...register(name)}
+              style={{ display: "none" }}
+              onClick={() => handleRating(ratingValue)}
+            />
+            <Icon
+              as={FaStar}
+              color={ratingValue <= (rating || hover) ? "yellow" : "grey.5"}
+              onMouseEnter={() => setHover(ratingValue)}
+              onMouseLeave={() => setHover(null)}
+              cursor={!disabled && "pointer"}
+              {...rest}
+            />
+          </label>
+        );
+      })}
+    </Flex>
+  );
+};
+
+export default StarRating;
