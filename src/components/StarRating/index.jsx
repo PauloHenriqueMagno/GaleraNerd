d26986550f2db145f4dfa3899ddd07aaeb500add
Sidney Patez <instalers.sa@gmail.com>
2021-11-16 09:42:03 -0300

    feat: errors feature

--- src/components/StarRating/index.jsx
+++ src/components/StarRating/index.jsx
@@ -1,5 +1,5 @@
 import { FaStar } from "react-icons/fa";
-import { Flex, Icon } from "@chakra-ui/react";
+import { Flex, Icon, Text } from "@chakra-ui/react";
 import { useState } from "react";
 
 const StarRating = ({
@@ -7,6 +7,7 @@ const StarRating = ({
   name,
   disabled = false,
   defaultValue = 0,
+  errorMessage,
   ...rest
 }) => {
   const [rating, setRating] = useState(defaultValue);
@@ -19,31 +20,36 @@ const StarRating = ({
   };
 
   return (
-    <Flex w="100%" justifyContent="space-between">
-      {[...Array(5)].map((star, index) => {
-        const ratingValue = index + 1;
+    <Flex direction="column">
+      <Flex w="100%" justifyContent="space-between">
+        {[...Array(5)].map((star, index) => {
+          const ratingValue = index + 1;
 
-        return (
-          <label key={ratingValue}>
-            <input
-              type="radio"
-              value={ratingValue}
-              name={name}
-              {...register(name)}
-              style={{ display: "none" }}
-              onClick={() => handleRating(ratingValue)}
-            />
-            <Icon
-              as={FaStar}
-              color={ratingValue <= (rating || hover) ? "yellow" : "grey.5"}
-              onMouseEnter={() => setHover(ratingValue)}
-              onMouseLeave={() => setHover(null)}
-              cursor={!disabled && "pointer"}
-              {...rest}
-            />
-          </label>
-        );
-      })}
+          return (
+            <label key={ratingValue}>
+              <input
+                type="radio"
+                value={ratingValue}
+                name={name}
+                {...register(name)}
+                style={{ display: "none" }}
+                onClick={() => handleRating(ratingValue)}
+              />
+              <Icon
+                as={FaStar}
+                color={ratingValue <= (rating || hover) ? "yellow" : "grey.5"}
+                onMouseEnter={() => setHover(ratingValue)}
+                onMouseLeave={() => setHover(null)}
+                cursor={!disabled && "pointer"}
+                {...rest}
+              />
+            </label>
+          );
+        })}
+      </Flex>
+      <Text fontSize="12px" color="error">
+        {errorMessage}
+      </Text>
     </Flex>
   );
 };
