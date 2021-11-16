2b9874d049f4fe6aff4a309ee8d31b5afe77b7d6
Matheus-Henrique-Burey <matheushburei.99@gmail.com>
2021-11-16 13:49:19 -0300

    feature/pages/register: debug

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,4 +1,4 @@
-import { Flex, Image, Button } from "@chakra-ui/react"
+import { Flex, Image, Button, Box } from "@chakra-ui/react"
 import RegisterForm from "../../components/RegisterForm";
 import svg from "../../assets/logoRG.svg";
 import {ArrowBackIcon} from "@chakra-ui/icons";
@@ -7,29 +7,33 @@ import { useHistory } from "react-router-dom";
 const Register = () => {
   const histori = useHistory();
   return (
-    <Flex 
-      align="center" 
-      justifyContent={{mobile:"center", desktop: "end"}} 
+    <Box
       minHeight="100vh" 
       bgGradient={{
         mobile: "linear(to-b, purple.2 35%, white 35%)",
         desktop: "linear(to-r, white 35%, purple.2 35%)"
       }}
     >
-      <Button 
-        p="4" 
-        position="absolute" 
-        top="15px" 
-        left="30px" fontSize="2rem" 
-        bg="purple.2" color="white" 
-        _hover={{backgroundColor: "purple.4"}}
-        onClick={() => histori.push('/')}
+        <Button 
+          mt="25"
+          ml="25"
+          p="4" 
+          fontSize="2rem" 
+          bg="purple.2" color="white" 
+          _hover={{backgroundColor: "purple.4"}}
+          onClick={() => histori.push('/')}
+        >
+          <ArrowBackIcon />
+        </Button>
+      <Flex 
+        align="center" 
+        justifyContent={{mobile:"center", desktop: "end"}} 
       >
-        <ArrowBackIcon />
-      </Button>
+
       <RegisterForm />
       <Image src={svg} display={{mobile: "none", desktop: "block"}} w={["0", "0", "35%", "40%"]} ml={["0", "0","5", "10"]} mr={["0", "0","0", "20"]} />
     </Flex>
+      </Box>
   )
 }
 
