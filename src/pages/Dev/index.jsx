0d0c0c575f8b89f9cef35a61a8b5495fd7de6b47
deborapavani <de.pavani@icloud.com>
2021-11-17 06:44:42 -0300

    feature: page created; waiting components to finish the style

--- src/pages/Dev/index.jsx
+++ src/pages/Dev/index.jsx
@@ -1,26 +1,26 @@
-// import DevForm from "../../components/DevForm";
+import DevForm from "../../components/DevForm";
 // import ProjectCard from "../../components/ProjectCard";
 import HeaderComponent from "../../components/Header";
-
-import { Box, Flex } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 
 const Dev = () => {
   return (
-    <Flex
-      align="center"
-      justifyContent={{ mobile: "center", desktop: "flex" }}
-      minHeight="100vh"
-      minWidth="100vw"
-      bgGradient={{
-        mobile: "linear(to-b, purple.2 35%, white 35%)",
-        desktop: "linear(to-b, purple.2 50%, white 50%, )",
-      }}
-    >
+    <>
       <HeaderComponent />
-      <Box>
-        {/* <DevForm /> */}
-      </Box>
-    </Flex>
+      <Flex
+        align="center"
+        justifyContent={{ mobile: "center", desktop: "flex-start" }}
+        minHeight="100vh"
+        minWidth="100vw"
+        bgGradient={{
+          mobile: "linear(to-b, purple.2 35%, white 35%)",
+          desktop: "linear(to-b, purple.2 50%, white 50%, )",
+        }}
+      >
+        <DevForm />
+        {/* <ProjectCard /> */}
+      </Flex>
+    </>
   );
 };
 
