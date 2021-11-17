07f2875fc262c4fd310ad902452e92a2db04bdec
PauloHenriqueMagno <paulohm2309@gmail.com>
2021-11-17 11:36:48 -0300

    fix: route to Home

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,18 +6,12 @@ import Search from "../pages/Search";
 import DevList from "../pages/DevsList";
 import Dev from "../pages/Dev";
 import ErrorPage from "../pages/ErrorPage";
-import ProjectCard from "../components/ProjectCard";
-import { Accordion } from "@chakra-ui/accordion";
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/">
-        <Accordion allowToggle>
-          <ProjectCard id="1" devId="2" projectId="1" description="Descrição" status="Aguardando orçamento"  />
-          <ProjectCard id="1" devId="2" projectId="1" description="Descrição" status="Aguardando orçamento"  />
-          <ProjectCard id="1" devId="2" projectId="1" description="Descrição" status="Aguardando orçamento"  />
-        </Accordion>
+        <Home />
       </Route>
       <Route path="/dev">
         <Dev />
